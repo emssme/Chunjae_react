11c07b5347aef0de729be27320fe03e955210ef6
emsseme <bk75121@gmail.com>
2023-09-20 16:54:02 +0900

    sass, open-color, include-media, styled-component

---
+++ ch04/src/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './Header.js';
+import Contents from './Contents.js';
+import Footer from './Footer.js'
+
+const Home = () => {
+  return (
+    <div className=''>
+        <Header />
+        <Contents />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
